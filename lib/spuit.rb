9948fecb6e6317e2b1d00988f0f4b28c65ff4dba
Yu Ishihara <ishihara.yu@gmail.com>
2017-06-26 22:09:08 +0900

    bugfix

--- lib/spuit.rb
+++ lib/spuit.rb
@@ -3,16 +3,16 @@
 dir = File.dirname(__FILE__)
 $LOAD_PATH.unshift dir unless $LOAD_PATH.include?(dir)
 
-require "oulu/generator"
-require "oulu/version"
+require "spuit/generator"
+require "spuit/version"
 require 'sass'
-require 'oulu'
+require 'spuit'
 
 unless defined?(Sass)
   require 'sass'
 end
 
-module Oulu
+module Spuit
   @components ||= []
   class << self
     attr_accessor :components
@@ -24,7 +24,7 @@ module Oulu
 
   if defined?(Rails) && defined?(Rails::Engine)
     class Engine < ::Rails::Engine
-      require 'oulu/engine'
+      require 'spuit/engine'
     end
 
     module Rails
@@ -35,7 +35,7 @@ module Oulu
       end
     end
   else
-    oulu_path = File.expand_path("../../app/assets/stylesheets", __FILE__)
-    ENV["SASS_PATH"] = [ENV["SASS_PATH"], oulu_path].compact.join(File::PATH_SEPARATOR)
+    spuit_path = File.expand_path("../../app/assets/stylesheets", __FILE__)
+    ENV["SASS_PATH"] = [ENV["SASS_PATH"], spuit_path].compact.join(File::PATH_SEPARATOR)
   end
 end
