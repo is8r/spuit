5724f88e78d113f85b9712c5af5f20a3145b1856
Yu Ishihara <ishihara.yu@gmail.com>
2018-12-23 16:21:06 +0900

    fix path

--- lib/spuit.rb
+++ lib/spuit.rb
@@ -35,7 +35,7 @@ module Spuit
       end
     end
   else
-    spuit_path = File.expand_path("../../app/assets/stylesheets", __FILE__)
+    spuit_path = File.expand_path("../../assets/stylesheets", __FILE__)
     ENV["SASS_PATH"] = [ENV["SASS_PATH"], spuit_path].compact.join(File::PATH_SEPARATOR)
   end
 end
