fd8b819a9d7d831f3577b86019e98ae805f214b2
Yu Ishihara <ishihara.yu@gmail.com>
2019-05-31 23:04:28 +0900

    Fix

--- lib/spuit.rb
+++ lib/spuit.rb
@@ -4,9 +4,9 @@ require "spuit/generator"
 module Spuit
   if defined?(Rails) && defined?(Rails::Engine)
     class Engine < ::Rails::Engine
-      config.assets.paths << File.expand_path("../scss", __FILE__)
+      config.assets.paths << File.expand_path("../../scss", __FILE__)
     end
   else
-    Sass.load_paths << File.expand_path("../scss", __FILE__)
+    Sass.load_paths << File.expand_path("../../scss", __FILE__)
   end
 end
--- lib/spuit/version.rb
+++ lib/spuit/version.rb
@@ -1,3 +1,3 @@
 module Spuit
-  VERSION = "0.0.14"
+  VERSION = "0.0.14.1"
 end
